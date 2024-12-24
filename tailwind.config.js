c2f2f7a0c5e11a4a550c56920d03b734545d5580
Tsuki <976499226@qq.com>
2024-12-24 15:40:54 +0800

    chore: init laravel-template

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+const {nextui} = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.tsx',
+        './resources/js/**/*.{js,ts,jsx,tsx}',
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: [...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    darkMode: "class",
+    plugins: [forms,nextui()],
+};
